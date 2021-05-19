17f3981a9f3c42f6b011cdd3b90e6551ca075e48
augustinaweb <a.radziunaite@gmail.com>
2021-05-19 20:51:25 +0300

    Revert "WIP 05/19"
    This reverts commit 3bf508ae553ccd72fad20ca9896fca21eb0bfbb1.

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import {
-	ApolloClient,
-	HttpLink,
-	InMemoryCache,
-	ApolloProvider
-} from '@apollo/client';
-
-const client = new ApolloClient({
-	link: new HttpLink({
-		uri: 'https://graphql-weather-api.herokuapp.com/'
-	}),
-	cache: new InMemoryCache()
-});
+// import 'regenerator-runtime/runtime'
 
 ReactDOM.render(
-	<ApolloProvider client={client}>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
-	</ApolloProvider>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>, 
 	document.getElementById('root')
 );
